77e9dc88727e6c71ffc00983795157916a7cbf2d
Chris Sewell <chrisj_sewell@hotmail.com>
2020-12-15 08:20:35 +0000

    ⬆️ UPGRADE: markdown-it-py < v2

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     ],
     keywords="markdown lexer parser development",
     python_requires="~=3.6",
-    install_requires=["markdown-it-py~=0.5.8"],
+    install_requires=["markdown-it-py>=0.5.8,<2.0.0"],
     extras_require={
         "code_style": ["pre-commit==2.6"],
         "testing": [
