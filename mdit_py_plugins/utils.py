b7a38d1196e7938842eeaa969026dfe37765aaf6
Chris Sewell <chrisj_sewell@hotmail.com>
2023-06-03 09:44:52 +0200

    ⬆️ UPGRADE: Allow markdown-it-py v3

--- mdit_py_plugins/utils.py
+++ mdit_py_plugins/utils.py
@@ -5,7 +5,7 @@ def is_code_block(state: StateBlock, line: int) -> bool:
     """Check if the line is part of a code block, compat for markdown-it-py v2."""
     try:
         # markdown-it-py v3+
-        return state.is_code_block(line)  # type: ignore[attr-defined]
+        return state.is_code_block(line)
     except AttributeError:
         pass
 
